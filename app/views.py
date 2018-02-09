0a27a4b76eb70a5bc8ee2f0fa457bdfa70e95674
peterwade153 <peterwade153@gmail.com>
2018-02-09 12:23:45 +0300

    fixing PEP8 issues

--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
+""" views.py"""
+from functools import wraps
+from werkzeug.security import check_password_hash
+from flask import render_template, request, redirect, url_for, session, flash
 from app import app
 from app.models import db, User, Business, Review
-from werkzeug.security import check_password_hash
-from functools import wraps
-from flask import render_template, request, redirect, url_for, session, flash
 
 
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'not_secret_any_more'
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:admin@localhost/weconnect"
 
@@ -24,30 +25,30 @@ def login_required(f):
 	return wrap
 
 
-@app.route('/', methods=['POST','GET'])
+@app.route('/', methods=['POST', 'GET'])
 def signup():
 	"""route registers a new user on the application"""
 
 	error = None
 	if request.method == "POST":
-		Username = request.form['username']
-		Email = request.form['email']
-		Password = request.form['password']
+		username = request.form['username']
+		email = request.form['email']
+		password = request.form['password']
 
 		#check if user doesnt exist!
-		New_user = User.query.filter_by(username = Username).first()
-		if New_user:
+		new_user = User.query.filter_by(username = username).first()
+		if new_user:
 			error = "User already exists!"
 			return render_template('signup.html', error = error)
 
-		Myuser = User(username = Username, email = Email, password = Password)
-		db.session.add(Myuser)
+		myuser = User(username = username, email = email, password = password)
+		db.session.add(myuser)
 		db.session.commit()
 
 		flash('New user registered!')
 		return redirect(url_for('login'))
 
-	return render_template('signup.html', error = error)
+	return render_template('signup.html',error = error)
 
 @app.route('/logout')
 def logout():
@@ -58,23 +59,23 @@ def logout():
 	return redirect(url_for('login'))
 
 
-@app.route('/login', methods=['POST','GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
 	"""route logins a user, to access the app"""
 
 	error = None
 	if request.method == 'POST':
-		Username = request.form['username']
-		Password = request.form['password']
+		username = request.form['username']
+		password = request.form['password']
 
-		Myuser = User.query.filter_by(username = Username).first()
+		myuser = User.query.filter_by(username = username).first()
 
-		if not Myuser:
+		if not myuser:
 			error = "username or password is incorrect, check and try again!"
         #check if it the right password passed
-		if check_password_hash(Myuser.password, Password):
+		if check_password_hash(myuser.password, password):
 			session['logged_in'] = True
-			Myuser_id = Myuser.id
+			myuser_id = myuser.id
 			return redirect(url_for('viewbusiness'))
 
 		error = "invalid username or password!"
@@ -86,15 +87,19 @@ def login():
 def viewbusiness():
 	""" routes enables user to see registered businesses """
 
-	All_businesses = Business.query.all()
-	return render_template('business.html', businesses = All_businesses)
+	all_businesses = Business.query.all()
+	return render_template('business.html',businesses = all_businesses)
 
 @app.route('/addbusiness')
 @login_required
 def addbusiness():
+	""" route enables user add a business"""
+
 	return render_template('addBusiness.html')
 
 @app.route('/reviews')
 @login_required
 def reviews():
+	""" routes enables users view reviews about businesses"""
+
 	return render_template('reviews.html')
